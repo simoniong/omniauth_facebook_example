f2b7a2e77d1d760b875d739584d555cd1707cb03
Simon Iong <manin.iong@gmail.com>
2014-04-21 20:51:15 +0800

    point root to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 OmniauthFacebookExample::Application.routes.draw do
-  get "home/index"
+  #get "home/index"
+  root "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
