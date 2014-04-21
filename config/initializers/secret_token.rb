304d46a232c3a44e1047bb4e3c51b9005be03926
Simon Iong <manin.iong@gmail.com>
2014-04-21 20:32:58 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmniauthFacebookExample::Application.config.secret_key_base = 'b4e0af4f564054a17a2ff1b90d531dfbcbe089b714b02b8492eb907f5c14fa571daf63e8eee8672465df170af77f15c0ec57fd3c44c6bdfa59ad1b6ff5237a08'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OmniauthFacebookExample::Application.config.session_store :cookie_store, key: '_omniauth_facebook_example_session'
